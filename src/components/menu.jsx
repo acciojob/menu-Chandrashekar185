0c1d32689ed47891e7ec062784c081cddb19ceb1
Chandrashekar185 <chanduminny32@gmail.com>
2025-08-04 21:32:52 +0530

    Create menu.jsx

---
+++ src/components/menu.jsx
@@ -0,0 +1,24 @@
+const Menu = ({ items, selectedCategory }) => {
+  const filteredItems =
+    selectedCategory === "All"
+      ? items
+      : items.filter((item) => item.category === selectedCategory);
+
+  return (
+    <div className="menu">
+      {filteredItems.map((item, index) => (
+        <div
+          key={index}
+          className="menu-item"
+          data-test-id={`menu-item-${item.category.toLowerCase()}`}
+        >
+          <h3>{item.name}</h3>
+          <img src={item.image} alt={item.name} />
+          <p>{item.price}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Menu;
