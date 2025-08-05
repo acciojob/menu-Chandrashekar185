1d2a64f4d6a4c8de8cd2bb9de899f90a6d713aa6
Chandrashekar185 <chanduminny32@gmail.com>
2025-08-05 12:54:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ const menuItems = [
     category: 'breakfast',
     price: 15.99,
     img: './images/item-1.jpeg',
-    desc: I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed ,
+    desc: `I am baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed`,
   },
   {
     id: 2,
@@ -18,7 +18,7 @@ const menuItems = [
     category: 'lunch',
     price: 13.99,
     img: './images/item-2.jpeg',
-    desc: vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats ,
+    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats`,
   },
   {
     id: 3,
@@ -26,7 +26,7 @@ const menuItems = [
     category: 'shakes',
     price: 6.99,
     img: './images/item-3.jpeg',
-    desc: ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.,
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
   },
   {
     id: 4,
@@ -34,7 +34,7 @@ const menuItems = [
     category: 'breakfast',
     price: 20.99,
     img: './images/item-4.jpeg',
-    desc: Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, ,
+    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut`,
   },
   {
     id: 5,
@@ -42,7 +42,7 @@ const menuItems = [
     category: 'lunch',
     price: 22.99,
     img: './images/item-5.jpeg',
-    desc: franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up ,
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up`,
   },
   {
     id: 6,
@@ -50,7 +50,7 @@ const menuItems = [
     category: 'shakes',
     price: 18.99,
     img: './images/item-6.jpeg',
-    desc: Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday,
+    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
   },
   {
     id: 7,
@@ -58,7 +58,7 @@ const menuItems = [
     category: 'breakfast',
     price: 8.99,
     img: './images/item-7.jpeg',
-    desc: carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird ,
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird`,
   },
   {
     id: 8,
@@ -66,7 +66,7 @@ const menuItems = [
     category: 'lunch',
     price: 12.99,
     img: './images/item-8.jpeg',
-    desc: on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  ,
+    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut`,
   },
   {
     id: 9,
@@ -74,9 +74,10 @@ const menuItems = [
     category: 'shakes',
     price: 16.99,
     img: './images/item-9.jpeg',
-    desc: skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.,
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
+
 export default function App() {
   const [selectedCategory, setSelectedCategory] = useState("All");
 
@@ -84,13 +85,13 @@ export default function App() {
     <div id="main">
       <h1>Menu</h1>
       <div className="btn-container">
-        <button id="filter-btn-1" onClick={() => setSelectedCategory("Breakfast")}>
+        <button id="filter-btn-1" onClick={() => setSelectedCategory("breakfast")}>
           Breakfast
         </button>
-        <button id="filter-btn-2" onClick={() => setSelectedCategory("Lunch")}>
+        <button id="filter-btn-2" onClick={() => setSelectedCategory("lunch")}>
           Lunch
         </button>
-        <button id="filter-btn-3" onClick={() => setSelectedCategory("Shakes")}>
+        <button id="filter-btn-3" onClick={() => setSelectedCategory("shakes")}>
           Shakes
         </button>
         <button id="filter-btn-all" onClick={() => setSelectedCategory("All")}>
